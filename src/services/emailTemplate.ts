cfa311882964c75bb6f3170a3ee6fa261c9b99c9
storm <khameleonstorm@gmail.com>
2025-04-05 11:04:00 +0100

    With

--- src/services/emailTemplate.ts
+++ src/services/emailTemplate.ts
@@ -6,7 +6,7 @@ export function emailTemplate(bodyContent: string) {
           <table role="presentation" width="100%" max-width="600px" bgcolor="#ffffff" style="border-radius: 8px; overflow: hidden; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
             <tr>
               <td align="center" style="padding: 20px; background: #13160F;">
-                <img src="https://i.postimg.cc/Qd0kb3wy/logo3.png" width="120" alt="Company Logo" style="display: block;">
+                <img src="https://res.cloudinary.com/ddb1vjioq/image/upload/v1743847285/logo3_nzla8r.png" width="120" alt="Company Logo" style="display: block;">
               </td>
             </tr>
 
